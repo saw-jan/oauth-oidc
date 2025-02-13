bbbf7b1b3c82fd2503e7cf293b5cb300c72757a1
Saw-jan <saw.jan.grg3e@gmail.com>
2025-02-13 17:50:45 +0545

    implement resource server

--- oauth2/resource-server/index.js
+++ oauth2/resource-server/index.js
@@ -1,7 +1,34 @@
 const express = require('express')
+const request = require('axios')
 
 const app = express()
 const port = 5001
+
+async function verifyToken(req, res, next) {
+  const token = '' // TODO: get token from request
+  const clientAuthToken = Buffer.from('system-client:top_secret_key').toString(
+    'base64'
+  )
+
+  const response = await request.post(
+    'http://localhost:5000/oauth/introspect',
+    { token },
+    { headers: { Authorization: `Basic ${clientAuthToken}` } }
+  )
+
+  if (response.status !== 200 || !response.data.active) {
+    res.status(401).send('Unauthorized')
+  }
+
+  next()
+}
+
+app.get('/api/files', verifyToken, (req, res) => {
+  res.json([
+    { id: 1, name: 'epsum.txt' },
+    { id: 2, name: 'lorem.txt' },
+  ])
+})
 
 app.all('*', (req, res, next) => {
   res.status(404).send('Not Found')
